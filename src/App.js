9c75babb955fa2b9b6ca8655850a3297f967310f
[cocosky77] <[skybella@naver.com]>
2024-09-13 15:53:51 +0900

    message

--- src/App.js
+++ src/App.js
@@ -3,21 +3,44 @@ import { useState } from 'react';
 import GameBox from './component/GameBox';
 
 function App() {
+    // const [isX, setIsX] = useState(null); /* 맨 처음 클릭 후 X 나오는 거 */
+    
+    // const isXClick = () => {  /* 기존 isXClick() */
+    //     setIsX("X"); 
+    // }
+
+    const [box, setBox] = useState(Array(9).fill(null)); /* 상자들의 상태(value)를 저장하는 배열 */
+    const [isNext, setIsNext] = useState(true);
+
+    const isXClick = (i) => { // 매개변수 i의 값에 따라 9개의 상자 중 업데이트 될 상자가 정해짐.
+      if (box[i]) {
+        return;
+      }
+      const nextBox = box.slice(); // nextBox라는 변수를 선언해 box 배열의 복제본을 넣음 // slice()사용해 복제한 배열의 일부를 발췌                                 
+      if(isNext) {
+        nextBox[i] = "X"; // 업데이트 될 상자의 첫값이 "X"로 정해짐(isNext를 true로 생성하였기 때문) 
+      }else{
+        nextBox[i] = "O";
+      };
+      setBox(nextBox); // useState의 set함수인 setBox에 nextBox를 넣어 업데이트된 값이 확정
+      setIsNext(!isNext); // isNext(true, false)값이 반전되고 state가 저장됨
+    }
+  
 
   return (
     <div className="App">
       <div className='gameBoard-container'>
         <p>TIC TAC TOE</p>
         <div className='gameBoard'> {/* 게임판 (300x300 크기) */}
-          <GameBox /> {/* 상자한칸, 값 -> 코드 너무 길어져서 컴포넌트로 뺌 */}
-          <GameBox />
-          <GameBox />
-          <GameBox />
-          <GameBox />
-          <GameBox />
-          <GameBox />
-          <GameBox />
-          <GameBox />
+          <GameBox value={box[0]} onBoxClick={() => isXClick(0)} /> {/* 상자한칸, 값 -> 코드 너무 길어져서 컴포넌트로 뺌 */}
+          <GameBox value={box[1]} onBoxClick={() => isXClick(1)} />
+          <GameBox value={box[2]} onBoxClick={() => isXClick(2)} />
+          <GameBox value={box[3]} onBoxClick={() => isXClick(3)} />
+          <GameBox value={box[4]} onBoxClick={() => isXClick(4)} />
+          <GameBox value={box[5]} onBoxClick={() => isXClick(5)} />
+          <GameBox value={box[6]} onBoxClick={() => isXClick(6)} />
+          <GameBox value={box[7]} onBoxClick={() => isXClick(7)} />
+          <GameBox value={box[8]} onBoxClick={() => isXClick(8)} />
         </div>
       </div>
     </div>
@@ -25,3 +48,23 @@ function App() {
 }
 
 export default App;
+
+/**
+ * <GameBox value={box[0]} onBoxClick={() => isXClick(0)}/>
+ * 
+ * const [box, setBox] = useState(Array(9).fill(null)); // 상자 value를 배열로
+ * const [isNext, setIsNext] = useState(true) // X,O 값
+ * 
+ * const isXClick = (i) => {
+ *  if(box[i]) { // 이미 상자에 value값이 있으면 return(변하지 못함)
+ *    return;
+ * };
+ * const nextBox = box.slice();
+ * if (isNext) {
+ * nextBox[i] = "X";
+ * } else{
+ * nextBox[i] = "O";
+ * }
+ * 
+ * }
+ */
--- src/component/GameBox.js
+++ src/component/GameBox.js
@@ -1,38 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./GameBox.css"
 
-const GameBox = () => {
-    const [isX, setIsX] = useState(false); /* 맨 처음 클릭 후 X 나오는 거(초깃값은 false) */
-    const [change, setChange] = useState(true);
-    const [value, setValue] = useState(Array(9).fill(null));
-
-    const isXClick = () => {
-        setIsX(true);
-        return;
-    }
+const GameBox = ({value, onBoxClick}) => {
     
-    const isChange = () => {
-        if(change === true) {
-                    setChange(false);
-                    return;
-                }else{
-                    setChange(true);
-                    return;
-                }
-    }
-
-    // useEffect(() => {
-    //     if(change === true) {
-    //         setChange(false);
-    //     }else{
-    //         setChange(true);
-    //     }
-    // },[change])
-
     return(
         <div>
-            <div className='gameBoard-box' onClick={isXClick}>
-            <div className='gameBoard-box_value' onClick={isChange}>{change ? (isX ? "X" : "") : "O"}</div>
+            <div className='gameBoard-box' onClick={onBoxClick}>
+            {/* <div className='gameBoard-box_value' onClick={isChange}>{change ? (isX ? "X" : "") : "O"}</div> */}
+            <div className='gameBoard-box_value'>{value}</div>
           </div>
         </div>
     );
@@ -40,8 +15,21 @@ const GameBox = () => {
 
 export default GameBox;
 
+
 /**
- * 상자를 클릭 -> "X"부터 시작 >> 완료!
+ * 상자를 클릭 -> 상자에 문자 "X" 입력 >> isXClick()
+ * 9개 상자 각각의 id가 없음 >> useState를 이용해 각 상자의 value를 지정하기 위해 box 배열 제작
+ * 각 상자 컴포넌트(<GameBox /> value에 box배열을 차례로 삽입함)
+ * 
+ * onBoxClick()를 onClick 메소드로 호출. [기존의 isXClick()와 연결시키기 위함]
+ * => isXClick()은 한 상자에 X클릭을 보여주는 함수지만, 부모 컴포넌트(App)에서 자식 컴포넌트에
+ * 일괄적으로 처리를 하기 위해 onBoxClick()을 만들어서 호출
+ * 
+ * 부모 컴포넌트(App)의 <GameBox />에서 onBoxClick()와 isXClick()을 연결
+ * 
+ * 기존 isXClick()와 다르게 수정된 isXClick()은 box[i]로 모든 상자를 업데이트할 수 있도록 수정
+ * 
+ * 
  * 클릭해서 X가 놓이면 그 다음 클릭은 무조건 O가 되어야 함(교차 반복)
  * X혹은 O이 연속해서 3개가 되면 게임 끝 => 게임판 아래에 우승자를 표시
  * 9칸이 다참(승패 결정X) => 게임판 아래에 "무승부"라고 표시
