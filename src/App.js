6c0bbc3de3664a0778d2d506919e17a325b9d88e
[cocosky77] <[skybella@naver.com]>
2024-09-26 16:49:14 +0900

    message

--- src/App.js
+++ src/App.js
@@ -1,103 +1,142 @@
-import './App.css';
-import GameBox from './component/GameBox';
-import { useState } from 'react';
+import "./App.css";
+import GameBox from "./component/GameBox";
+import { useState } from "react";
 
 function App() {
-  const [box, setBox] = useState(Array(9).fill(null)); /* 상자들의 상태(value)를 저장하는 배열 */
-  const [nextBox, setNextBox] = useState(true); // 초깃값 설정과 X,O 교차를 위한 리액트훅
-  const copyBox = box.slice(); // nextBox라는 변수를 선언해 box 배열의 복제본을 넣음 // slice()사용해 복제한 배열의 일부를 발췌 
-  const winner = calculateWinner(box); // 우승자
+  // history: square 배열을 감싸는 배열 ex) [{...}, {...}, {...},];
+  // square: 1개의 배열 ex) [null, null, "X", null ...];
+  const [history, setHistory] = useState([{ square: Array(9).fill(null) }]);
+  const [nextBox, setNextBox] = useState(true); // X,O 교차를 위함
+  const [travel, setTravel] = useState(0); // 과거로의 시간여행을 위함
 
-  const isClick = (i) => {  // 매개변수 i의 값에 따라 9개의 상자 중 업데이트 될 상자가 정해짐.
-    if(copyBox[i] || winner){ // 상자에 이미 값이 있거나 우승자가 확정되었을 경우 끝내기
+  const calculateWinner = (item) => {
+    const numberOfCases = [
+      // 모든 승리의 경우(가로, 세로, 대각선)
+      [0, 1, 2], // 첫 번째 행
+      [3, 4, 5], // 두 번째 행
+      [6, 7, 8], // 세 번째 행
+      [0, 3, 6], // 첫 번째 열
+      [1, 4, 7], // 두 번째 열
+      [2, 5, 8], // 세 번째 열
+      [0, 4, 8], // 첫 번째 대각선
+      [2, 4, 6], // 두 번째 대각선
+    ];
+
+    for (let i = 0; i < numberOfCases.length; i++) {
+      const [a, b, c] = numberOfCases[i]; // 승리 조합의 인덱스 값을 꺼내 a,b,c에 구조 분해 할당
+      if (item[a] && item[a] === item[b] && item[a] === item[c]) {
+        return item[a]; // 승자 반환("X" or "O")
+      }
+    }
+    return null; // 승자가 없으면 null 반환
+  };
+
+  const current = history[history.length - 1]; // 현재 상태의 배열(가장 최근 상태)
+  const winner = calculateWinner(current.square); // 현재 상태의 우승자
+
+  const isClick = (i) => {
+    console.log("history", history); // 클릭 > 업데이트되는 history 상태
+
+    const copyBox = current.square.slice(); // 현재 상태의 배열 복사본(가장 최근 상태 복사본)
+
+    // 상자에 이미 값이 있거나 우승자가 확정되었을 경우 끝내기
+    if (copyBox[i] || winner) {
       return;
-    };
+    }
 
-    if(nextBox) {
-      copyBox[i] = "X"; // 업데이트 될 상자의 첫값이 "X"로 정해짐(isNext를 true로 생성하였기 때문) 
-    }else{
-      copyBox[i] = "O";
-    };
+    copyBox[i] = nextBox ? "X" : "O"; // "X", "O" 교차
 
-    setBox(copyBox);  // useState의 set함수인 setBox에 nextBox를 넣어 업데이트된 값이 확정
-    setNextBox(!nextBox); // isNext(true, false)값이 반전되고 state가 저장됨
+    // const goBack = [...history, { square: copyBox }];
+
+    setHistory([...history, { square: copyBox }]); // set함수에 업데이트된 배열 상태 저장
+    setNextBox(!nextBox); // true, false 값이 반전되고 state가 저장됨
+    setTravel(history.length); // 현재 길이 저장(즉 travel에 배열 길이가 저장됨)
   };
 
-  let result;
   let isDraw = true; // null이 없는 경우에만 true가 됨
+  let result;
 
-  if(winner) {
-    result = `Winner is ${winner}`;
-    isDraw = ""; // 우승자가 나왔을 경우 무승부 표시X(마지막에 동시에 나오는 경우 방지 차원)
-  }else{
-    result = "";
-  };
+  switch (winner) {
+    case null:
+      result = `Next turn is ${nextBox ? "X" : "O"}`;
+      break; // switch case문에서 필수!
 
+    default:
+      result = `Winner is ${winner}`;
+      isDraw = ""; // 우승자가 나왔을 경우 무승부 표시X(마지막에 동시에 나오는 경우 방지 차원)
+      break;
+  }
 
-  for(let i=0; i<box.length; i++) {
-    if(box[i] === null) { // 한 칸이라도 null이 있으면 isDraw false(무승부X) => 상자가 다 채워져야만 무승부
+  for (let i = 0; i < current.square.length; i++) {
+    // 한 칸이라도 null이 있으면 isDraw false(무승부X) => 상자가 다 채워져야만 무승부O
+    if (current.square[i] === null) {
       isDraw = false;
       break;
     }
+  }
+
+  const timeTravel = (i) => {
+    setTravel(i); // 이동하려는 i로 설정하기
+    setHistory(history.slice(0, i + 1));
+    setNextBox(!nextBox); // X, O 값 교차 => 한 단계씩 과거로 가기 때문
+    // const travel = history[history] - current;
+    //  = current.square.slice(); // 현재
+
+    // if (now.history[history.length] > 0) {
+    //   // travel = now.history[history.length - 1];
+
+    // } else {
+    // }
+
+    // const pastpast = now.square.slice();
+    // const past = history[history.length - 2]; // 현재 배열 -1(한 칸 과거로)
+
+    // const pastpast = gopast.square.slice(); // past배열 복사본
+
+    // setHistory([...history, travel]);
   };
-  
-  if(isDraw) {
-    isDraw = "It's a DRAW!"
-  }else{
-    isDraw = ""
+
+  if (isDraw) {
+    isDraw = "It's a DRAW!";
+    result = "";
+  } else {
+    isDraw = "";
   }
 
   const reset = () => {
-    setBox(Array(9).fill(null));
+    setHistory([{ square: Array(9).fill(null) }]); // 초기화
     setNextBox(true);
-  }
+  };
 
-  
   return (
     <div className="App">
-      <div className='gameBoard-container'>
+      <div className="gameBoard-container">
         <p>TIC TAC TOE</p>
-        <div className='gameBoard'> {/* 게임판 (300x300 크기) */}
-          <GameBox value={box[0]} onBoxClick={() => isClick(0)}/> {/* 상자한칸, 값 -> 코드 너무 길어져서 컴포넌트로 뺌 */}
-          <GameBox value={box[1]} onBoxClick={() => isClick(1)}/>
-          <GameBox value={box[2]} onBoxClick={() => isClick(2)}/>
-          <GameBox value={box[3]} onBoxClick={() => isClick(3)}/>
-          <GameBox value={box[4]} onBoxClick={() => isClick(4)}/>
-          <GameBox value={box[5]} onBoxClick={() => isClick(5)}/>
-          <GameBox value={box[6]} onBoxClick={() => isClick(6)}/>
-          <GameBox value={box[7]} onBoxClick={() => isClick(7)}/>
-          <GameBox value={box[8]} onBoxClick={() => isClick(8)}/>
+        {/* 게임판 (300x300 크기) */}
+        <div className="gameBoard">
+          <GameBox value={current.square[0]} onBoxClick={() => isClick(0)} />
+          <GameBox value={current.square[1]} onBoxClick={() => isClick(1)} />
+          <GameBox value={current.square[2]} onBoxClick={() => isClick(2)} />
+          <GameBox value={current.square[3]} onBoxClick={() => isClick(3)} />
+          <GameBox value={current.square[4]} onBoxClick={() => isClick(4)} />
+          <GameBox value={current.square[5]} onBoxClick={() => isClick(5)} />
+          <GameBox value={current.square[6]} onBoxClick={() => isClick(6)} />
+          <GameBox value={current.square[7]} onBoxClick={() => isClick(7)} />
+          <GameBox value={current.square[8]} onBoxClick={() => isClick(8)} />
         </div>
-        {/* <div className='gameBoard-result'>{winner ? `winner is ${winner}` : `draw`}</div> */}
-        <div className='gameBoard-result'>
+        <div className="gameBoard-result">
           <div>{result}</div>
           <div>{isDraw}</div>
           <button onClick={reset}>Reset</button>
         </div>
       </div>
+      <div className="gameBoard-button">
+        <button onClick={() => timeTravel(travel - 1)}>Go to back!</button>
+        {/* <button onClick={() => timeTravel(travel - 2)}>Go to back!</button>
+        <button onClick={() => timeTravel(travel - 3)}>Go to back!</button> */}
+      </div>
     </div>
   );
-}
-
-const calculateWinner = (box) => {
-  const list = [ // 모든 승리의 경우(가로/세로/대각선)
-    [0, 1, 2], // 첫 번째 행
-    [3, 4, 5], // 두 번째 행
-    [6, 7, 8], // 세 번째 행
-    [0, 3, 6], // 첫 번째 열
-    [1, 4, 7], // 두 번째 열
-    [2, 5, 8], // 세 번째 열
-    [0, 4, 8], // 첫 번째 대각선
-    [2, 4, 6], // 두 번째 대각선
-  ];
-
-  for (let i=0; i<list.length; i++){
-    const [a,b,c] = list[i];  // 승리 조합의 인덱스 값을 꺼내 a,b,c에 구조 분해 할당
-    if(box[a] && box[a] === box[b] && box[a] === box[c]) {
-      return box[a]; // 승자 반환
-    }
-  }
-  return null; // 승자가 없으면 null 반환
 }
 
 export default App;
--- src/component/GameBox.js
+++ src/component/GameBox.js
@@ -1,40 +1,39 @@
-import "./GameBox.css"
+import "./GameBox.css";
 
-const GameBox = ({value, onBoxClick}) => {
-    
-    return(
-        <div>
-            <div className='gameBoard-box' onClick={onBoxClick}>
-            {/* <div className='gameBoard-box_value' onClick={isChange}>{change ? (isX ? "X" : "") : "O"}</div> */}
-            <div className='gameBoard-box_value'>{value}</div>
-          </div>
-        </div>
-    );
-}
+const GameBox = ({ value, onBoxClick }) => {
+  return (
+    <div>
+      <div className="gameBoard-box" onClick={onBoxClick}>
+        {/* <div className='gameBoard-box_value' onClick={isChange}>{change ? (isX ? "X" : "") : "O"}</div> */}
+        <div className="gameBoard-box_value">{value}</div>
+      </div>
+    </div>
+  );
+};
 
 export default GameBox;
 
-
 /**
- * 
+ *
+ *
  * 상자를 클릭 -> 상자에 문자 "X" 입력 >> isClick()
  * 9개 상자 각각의 id가 없음 >> useState를 이용해 각 상자의 value를 지정하기 위해 box 배열 제작
  * 각 상자 컴포넌트(<GameBox /> value에 box배열을 차례로 삽입함)
- * 
+ *
  * 각 상자 컴포넌트(GameBox value box 차례로 삽입)
  * onBoxClick()를 onClick 메소드로 호출. [기존의 isClick()와 연결시키기 위함]
  * => isClick()은 한 상자에 X클릭을 보여주는 함수지만, 부모 컴포넌트(App)에서 자식 컴포넌트에
  * 일괄적으로 처리를 하기 위해 onBoxClick()을 만들어서 호출
- * 
+ *
  * 부모 컴포넌트(App)의 <GameBox />에서 onBoxClick()와 isClick()을 연결
- * 
+ *
  * 기존 isClick()와 다르게 수정된 isClick()은 box[i]로 모든 상자를 업데이트할 수 있도록 수정
- * 
- * 
+ *
  * 클릭해서 X가 놓이면 그 다음 클릭은 무조건 O가 되어야 함(교차 반복)
- * 
+ *
  * X혹은 O이 연속해서 3개가 되면 게임 끝 => 게임판 아래에 우승자를 표시
  * 9칸이 다참(승패 결정X) => 게임판 아래에 "무승부(It's a DRAW!)"라고 표시
- * 
- * 
+ *
+ * 스프레드 연산자[...]
+ *
  */
